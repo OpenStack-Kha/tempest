3967be5ff92271e6c5814da1cab09630d424215f
Nikolay Pliashechnikov <npliashechnikov@mirantis.net>
2012-08-21 12:22:02 +0300

    Signed-off-by: Nikolay Pliashechnikov <npliashechnikov@mirantis.net>
       Added possibility to execute test script on openstack AIO host.
       Added image caching to reduce network traffic.
       Various bugfixes.
    
    	modified:   tempest/tests/compute/test_image_access_privacy.py

--- tempest/tests/compute/test_image_access_privacy.py
+++ tempest/tests/compute/test_image_access_privacy.py
@@ -1,10 +1,13 @@
+import unittest
 from tempest.openstack import Manager, AdminManager
+
+import tempest
 
 from tempest.services.network.json.network_client import NetworkClient
 from tempest.services.identity.json.admin_client import AdminClient, TokenClient
 from tempest.tests.compute.base import BaseComputeTest
 
-
+import json
 import fabric.api
 
 #Detailed test design.
@@ -14,28 +17,32 @@ import fabric.api
 # 4. Create network network2, ip range 10.0.3.0/24, vlan 102, project tenant2.
 # 5. Create user user1/password1, tenant tenant1.
 # 6. Create user user2/password2, tenant tenant2.
-# 7. As user user1: clone server image on tenant1 with x-image-meta-is-public = False.
-# 8. Get a reference to the cloned image.
-# 9. As user user2: try to retrieve the cloned image.
-# 10. Verify that error 403 was thrown at this attempt.
-# 11. As user user1: delete the cloned image.
-# 12. Delete tenants tenant1 and tenant2.
+# 7. Grant Member roles to the users.
+# 8. Create and upload novarc_userX files.
+# 9. Check cirros-0.3.0 image and upload it if not present.
+# 10. As user user1: Create new glance image and retrieve its id.
+# 11. As user user2: try to delete the image by id.
+# 12. Verify that deletion error has occured.
+# 13. As user user1: try to delete the image by id.
+# 14. Verify that correct image was deleted.
+# 15. Clean environment up - delete created tenants and users.
 
-class TestImageAccessSecurity(BaseComputeTest):
+class TestImageAccessPrivacy(BaseComputeTest):
     
     @classmethod
     def setUpClass(cls):
-        super(cls)
-        cls.admin_mgr = AdminManager()
+        super(TestImageAccessPrivacy, cls).setUpClass()
+        
         cls.config = tempest.config.TempestConfig()
         cls.username = cls.config.identity_admin.username
 
         cls.password = cls.config.identity_admin.password
-        cls.tenant_name = cls.config.identity_admin.tenant_name
+        cls.tenant_name = 'openstack'
         cls.admin_client = AdminClient(cls.config, cls.username, cls.password, cls.config.identity.auth_url, cls.tenant_name)
         
         cls.TENANT1_NAME = "tenant1"
         cls.TENANT2_NAME = "tenant2"
+        
         cls.USER1_NAME = "user1"
         cls.USER1_PSWD = "password1"
         
@@ -49,73 +56,74 @@ class TestImageAccessSecurity(BaseComputeTest):
         try:
             self.admin_client.delete_user(self.user1_id)
             self.admin_client.delete_user(self.user2_id)
-            self.admin_client.delete_tenant(self.tenant2_id)
+            self.admin_client.delete_tenant(self.tenant1_id)
             self.admin_client.delete_tenant(self.tenant2_id)
         except:
             pass
-        
-    def testCase1(self):
+    
+    #@attr(type='positive')
+    def test_case1(self):
         #1. Create two tenants.
         resp, body = self.admin_client.create_tenant(self.TENANT1_NAME)
         self.tenant1_id = body["id"]
         resp, body = self.admin_client.create_tenant(self.TENANT2_NAME)
         self.tenant2_id = body["id"]
         
-        #2. Create two users (one per each tenant).
+        #2. Create and enable two users (one per each tenant).
         resp, body = self.admin_client.create_user(self.USER1_NAME, self.USER1_PSWD, self.tenant1_id, "example1@example.com")
         self.user1_id = body["id"]
+        resp, body = self.admin_client.enable_disable_user(self.user1_id, True)
         resp, body = self.admin_client.create_user(self.USER2_NAME, self.USER2_PSWD, self.tenant2_id, "example2@example.com")
         self.user2_id = body["id"]
+        resp, body = self.admin_client.enable_disable_user(self.user2_id, True)
         
         #3. Get ID of Member role from keystone.
         resp, body = self.admin_client.list_roles()
-        self.admin_role_id = body["Member"]["id"]
+        for role in body:
+            if role["name"] == "Member":
+                self.member_role_id = role["id"]
         
-        #4. Assign tenant-admin role for each user in their tenants.
-        resp, body = self.admin_client.assign_user_role(self.user1_id, self.admin_role_id, self.tenant1_id)
-        resp, body = self.admin_client.assign_user_role(self.user2_id, self.admin_role_id, self.tenant2_id)
+        #4. Assign tenant-member role for each user in their tenants.
+        resp, body = self.admin_client.assign_user_role(self.user1_id, self.member_role_id, self.tenant1_id)
+        resp, body = self.admin_client.assign_user_role(self.user2_id, self.member_role_id, self.tenant2_id)
         
         #prepare fabric.
-        fabric.api.env["host_string"] = "root@192.168.123.4"
-        fabric.api.env["password"] = "ZofXB3AP"
         
-        self.vm_ip = "192.168.122.3"
-        self.vm_pass = "password"
-        
-        fabric.api.run("socat TCP4-LISTEN:2222 TCP4:192.168.123.4:22 &")
-        fabric.api.env["host_string"] = "root@192.168.123.4:2222"
-        fabric.api.env["password"] = self.vm_pass
-        
-        
+        fabric.api.env["host_string"] = "%s@%s:%s"%(self.config.identity_admin.host_user, self.config.identity.host, self.config.identity_admin.host_ssh_port)
+        fabric.api.env["password"] = self.config.identity_admin.host_password
+                
         #upload latest cirros image to a target machine.
-        fabric.api.run("wget -q https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-i386-disk.img /tmp")
+        output = fabric.api.run("ls ~")
+        if not "cirros-0.3.0-i386-disk.img" in output:
+            fabric.api.run("wget -q https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-i386-disk.img")
         
         f = file("/tmp/novarc_user1", "wt")
-        f.writelines(["export OS_USERNAME=%s"%self.USER1_NAME, "export OS_PASSWORD=%s"%self.USER1_PSWD, \
-                      "export OS_TENANT_NAME=%s"%self.TENANT1_NAME, "export OS_AUTH_URL=http://127.0.0.1:35357/v2.0/"])
+        f.writelines(["export OS_USERNAME=%s\n"%self.USER1_NAME, "export OS_PASSWORD=%s\n"%self.USER1_PSWD, \
+                      "export OS_TENANT_NAME=%s\n"%self.TENANT1_NAME, "export OS_AUTH_URL=http://127.0.0.1:35357/v2.0/\n"])
         f.close()
         
         f = file("/tmp/novarc_user2", "wt")
-        f.writelines(["export OS_USERNAME=%s"%self.USER2_NAME, "export OS_PASSWORD=%s"%self.USER2_PSWD, \
-                      "export OS_TENANT_NAME=%s"%self.TENANT2_NAME, "export OS_AUTH_URL=http://127.0.0.1:35357/v2.0/"])
+        f.writelines(["export OS_USERNAME=%s\n"%self.USER2_NAME, "export OS_PASSWORD=%s\n"%self.USER2_PSWD, \
+                      "export OS_TENANT_NAME=%s\n"%self.TENANT2_NAME, "export OS_AUTH_URL=http://127.0.0.1:35357/v2.0/\n"])
         f.close()
         
-        fabric.api.put("/tmp/novarc_user*", "/tmp")
+        output = fabric.api.run("ls /tmp")
+        if not ("novarc_user1" in output) and ("novarc_user2" in output):
+            fabric.api.put("~/novarc_user*", "/tmp")
         
         output = fabric.api.run('source /tmp/novarc_user1 && glance add name="Cirros_user1_private" disk_format=qcow2 '\
-                                'container_format=ovf is_public=False < /tmp/cirros-0.3.0-i386-disk.img')
+                                'container_format=ovf is_public=False < ~/cirros-0.3.0-i386-disk.img')
         image_id = ""
-        image_id = output[-1].split(': ')[-1]
+        image_id = output.split(': ')[-1]
         
+        fabric.api.env["warn_only"] = True
         output = fabric.api.run('source /tmp/novarc_user2 && glance -f delete %s'%image_id)
         
-        self.assertTrue(output[-1] == 'Not deleting image %s'%image_id)
+        self.assertTrue('No image with ID %s was found'%image_id in output)
         
-        output = fabric.api.run('source /tmp/novarc_user2 && glance -f delete %s'%image_id)
+        output = fabric.api.run('source /tmp/novarc_user1 && glance -f delete %s'%image_id)
         
-        self.assertTrue(output[-1] == "Deleted image %s"%image_id)
-        
-        self.admin_client.delete_user(self.user1_id)
-        self.admin_client.delete_user(self.user2_id)
-        self.admin_client.delete_tenant(self.tenant2_id)
-        self.admin_client.delete_tenant(self.tenant2_id)
+        self.assertTrue("Deleted image %s"%image_id in output)
+
+if __name__ == "__main__":
+    unittest.main()
